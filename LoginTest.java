535bac62fe91120968971439b42ff038c35945ff
Gomathi-92 <gomathircse@gmail.com>
2026-06-02 18:12:27 +0530

    Add files via upload

---
+++ LoginTest.java
@@ -0,0 +1,32 @@
+package SeleniumTestingProject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com");
+
+        driver.findElement(By.id("user-name"))
+              .sendKeys("standard_user");
+
+        driver.findElement(By.id("password"))
+              .sendKeys("secret_sauce");
+
+        driver.findElement(By.id("login-button"))
+              .click();
+
+        String title = driver.getTitle();
+
+        if(title.contains("Swag Labs")) {
+            System.out.println("Login Successful");
+        }
+
+        driver.quit();
+    }
+}
